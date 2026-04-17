9d761cce9b63e7b8eea23464671663c724c7b612
moy8a <moises8a0957@gmail.com>
2026-04-16 22:02:49 -0600

    ahora arregle mi cagada

--- reportes/verReportes.js
+++ reportes/verReportes.js
@@ -4,29 +4,56 @@ import { soloColaborador } from "../autenticacion.js";
 soloColaborador(function datosUsuario(){});
 //
 
+
+
+let tabActiva = 'pendiente';
+
+/*function mostrarTab(tab, btn) {
+    document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
+    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+    document.getElementById('tab-' + tab).classList.add('active');
+    btn.classList.add('active');
+    tabActiva = tab;
+}*/
 let pendiente = document.getElementById("pendiente");
+let tabPendiente = document.getElementById("tab-pendiente");
+
+
 let enProceso = document.getElementById("enProceso");
+let tabProceso = document.getElementById("tab-enproceso");
+
 let resuelto = document.getElementById("resuelto");
+let tabCompletado = document.getElementById("tab-completado");
 
 function ocultarTab(){
     pendiente.classList.remove('active');
     enProceso.classList.remove('active');
     resuelto.classList.remove('active');
+
+    tabPendiente.classList.remove('active');
+    tabProceso.classList.remove('active');
+    tabCompletado.classList.remove('active');
 }
 
 pendiente.addEventListener('click', function(){
     ocultarTab();
     pendiente.classList.add('active');
+    tabActiva = 'pendiente';
+    tabPendiente.classList.add('active');
 })
 
 enProceso.addEventListener('click', function(){
     ocultarTab();
     enProceso.classList.add('active');
+    tabActiva = 'enproceso';
+    tabProceso.classList.add('active');
 })
 
 resuelto.addEventListener('click', function(){
     ocultarTab();
     resuelto.classList.add('active');
+    tabActiva = 'completado';
+    tabCompletado.classList.add('active');
 })
 
 /////
@@ -40,6 +67,10 @@ function exportarExcel() {
     XLSX.utils.book_append_sheet(wb, ws, nombres[tabActiva]);
     XLSX.writeFile(wb, 'Reportes_' + nombres[tabActiva] + '.xlsx');
 }
+
+document.getElementById("exportarExcel").addEventListener('click', function(){
+    exportarExcel();
+})
 
 //Boton perfil
 const btn = document.querySelector(".btnPerfil");
