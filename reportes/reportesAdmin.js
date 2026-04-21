ff0098cbb70554afed0b1e9d561436f2eab50761
moy8a <moises8a0957@gmail.com>
2026-04-20 20:02:03 -0600

    agregar botones y arreglar duplicacion de reportes

--- reportes/reportesAdmin.js
+++ reportes/reportesAdmin.js
@@ -4,8 +4,13 @@ import { cargarReportes } from "../crearReporte.js";
 
 //solo permitir a colaboradores y admins con sesion iniciada
 soloColaborador(function datosUsuario() {
-    
+  
   cargarReportes(function (arregloReportes) {
+    //limpiar tabla
+    document.getElementById("Tabla_pendiente").innerHTML = "";
+    document.getElementById("Tabla_proceso").innerHTML = "";
+    document.getElementById("Tabla_completado").innerHTML = "";
+    
     arregloReportes.forEach(datosReporte => {
       
       const estado = datosReporte.estado;
@@ -28,13 +33,32 @@ soloColaborador(function datosUsuario() {
   });
 });
 
+//llenar las tablas con datos correspondientes
 function elementosTabla(datosReporte, tablaActiva) {
   
   //crear una fila por reporte
   let filaTabla = document.createElement('tr');
   tablaActiva.append(filaTabla);
   
-  //objeto con solo los datos a mostrar en la tabla pendiente
+  //agregar botones solo en tabla pendiente y tabla en proceso
+  const idTabla = tablaActiva.getAttribute("id")
+  let botonReporte = document.createElement('button');
+  let tdBoton = document.createElement('td');
+  tdBoton.append(botonReporte);
+  
+  if(idTabla === "Tabla_pendiente" || idTabla === "Tabla_proceso"){
+    botonReporte.classList.add("btn-accion");
+    
+    if (idTabla === "Tabla_pendiente") {
+      botonReporte.classList.add("btn-atender")
+      botonReporte.textContent = "Atender"
+    } else {
+      botonReporte.classList.add("btn-completar")
+      botonReporte.textContent = "Completar"
+    }
+  }
+  
+  //objeto con solo los datos a mostrar en la tabla
   let fecha_expedicion = (datosReporte.fecha_expedicion.toDate().toString().slice(0, 24))
   let datos = {
     id: datosReporte.numero_reporte,
@@ -44,7 +68,7 @@ function elementosTabla(datosReporte, tablaActiva) {
     edificio: datosReporte.edificio,
     descripcion: datosReporte.descripcion,
     codigoA: datosReporte.codigo_usuario,
-    codigoC: "N/A",
+    codigoC: datosReporte.codigo_colaborador,
     area: datosReporte.area
   };
   
@@ -53,5 +77,8 @@ function elementosTabla(datosReporte, tablaActiva) {
     let datoTabla = document.createElement('td')
     datoTabla.textContent = dato;
     filaTabla.append(datoTabla);
+    if (idTabla === "Tabla_pendiente" || idTabla === "Tabla_proceso") {
+      filaTabla.append(tdBoton);
+    }
   });
 }
